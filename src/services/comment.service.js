ff988dc8910e40fd0dac469e714b212f80537135
quangtuanitmo18 <quangtuanitmo18@gmail.com>
2023-09-21 17:18:18 +0300

    nested comment

--- src/services/comment.service.js
+++ src/services/comment.service.js
@@ -1,7 +1,7 @@
-const Comment = require('../models/comment.model')
-const {convert2ObjectId} = require("../utils");
-const {Api404Error} = require("../core/error.response");
-const {ProductService} = require("./product.service");
+const Comment = require("../models/comment.model");
+const { convert2ObjectId } = require("../utils");
+const { Api404Error } = require("../core/error.response");
+const { ProductService } = require("./product.service");
 
 /**
  * key features: Comment service
@@ -10,144 +10,167 @@ const {ProductService} = require("./product.service");
  * - delete a comment [User, Shop , Admin]
  */
 class CommentService {
-    static async createComment({
-        productId, userId, content, parentCommentId = null
-                               }) {
-        await this.validateProductExists(productId)
+  static async createComment({
+    productId,
+    userId,
+    content,
+    parentCommentId = null,
+  }) {
+    await this.validateProductExists(productId);
 
-        const comment = new Comment({
-            comment_product_id: productId,
-            comment_user_id: userId,
-            comment_content: content,
-            comment_parent_id: parentCommentId
-        })
+    const comment = new Comment({
+      comment_product_id: productId,
+      comment_user_id: userId,
+      comment_content: content,
+      comment_parent_id: parentCommentId,
+    });
 
-        let rightValue = 0
-        if (parentCommentId) {
-            // reply comment
-            const parentComment = await Comment.findById(parentCommentId)
-            if (!parentComment) throw new Api404Error('Parent comment not found')
+    let rightValue = 0;
+    if (parentCommentId) {
+      // reply comment
+      const parentComment = await Comment.findById(parentCommentId);
+      if (!parentComment) throw new Api404Error("Parent comment not found");
 
-            rightValue = parentComment.comment_right
+      rightValue = parentComment.comment_right;
 
-            // updateMany comments
-            await Comment.updateMany({
-                comment_product_id: convert2ObjectId(productId),
-                comment_right: { $gte: rightValue}
-            }, {
-                $inc: {comment_right: 2}
-            })
-
-            // updateMany comments
-            await Comment.updateMany({
-                comment_product_id: convert2ObjectId(productId),
-                comment_left: { $gt: rightValue}
-            }, {
-                $inc: {comment_left: 2}
-            })
-        } else {
-            const maxRightValue = await Comment.findOne({
-               comment_product_id: convert2ObjectId(productId)
-            }, 'comment_right', {sort: {comment_right: -1}})
-            if (maxRightValue) {
-                rightValue = maxRightValue.comment_right + 1
-            } else {
-                rightValue = 1
-            }
+      // updateMany comments
+      await Comment.updateMany(
+        {
+          comment_product_id: convert2ObjectId(productId),
+          comment_right: { $gte: rightValue },
+        },
+        {
+          $inc: { comment_right: 2 },
         }
+      );
 
-        console.log('rightValue::', rightValue)
-        // insert to comment
-        comment.comment_left = rightValue
-        comment.comment_right = rightValue + 1;
-
-        await comment.save()
-        return comment
+      // updateMany comments
+      await Comment.updateMany(
+        {
+          comment_product_id: convert2ObjectId(productId),
+          comment_left: { $gt: rightValue },
+        },
+        {
+          $inc: { comment_left: 2 },
+        }
+      );
+    } else {
+      const maxRightValue = await Comment.findOne(
+        {
+          comment_product_id: convert2ObjectId(productId),
+        },
+        "comment_right",
+        { sort: { comment_right: -1 } }
+      );
+      if (maxRightValue) {
+        rightValue = maxRightValue.comment_right + 1;
+      } else {
+        rightValue = 1;
+      }
     }
 
-    static async getCommentsByParentId({
-        productId,
-        parentCommentId = null,
-        limit = 50,
-        offset = 0
-    }){
-        if (parentCommentId) {
-            const parent = await Comment.findById(parentCommentId)
-            if (!parent) throw new Api404Error('Not found comment for product')
+    console.log("rightValue::", rightValue);
+    // insert to comment
+    comment.comment_left = rightValue;
+    comment.comment_right = rightValue + 1;
 
-            return Comment.find({
-                comment_product_id: convert2ObjectId(productId),
-                comment_left: {$gt: parent.comment_left},
-                comment_right: {$lte: parent.comment_right}
-            }).select({
-                comment_left: 1,
-                comment_right: 1,
-                comment_content: 1,
-                comment_parent_id: 1
-            }).sort({
-                comment_left: 1
-            });
-        }
+    await comment.save();
+    return comment;
+  }
 
-        return Comment.find({
-            comment_product_id: convert2ObjectId(productId),
-            comment_parent_id: parentCommentId
-        }).select({
-            comment_left: 1,
-            comment_right: 1,
-            comment_content: 1,
-            comment_parent_id: 1
-        }).sort({
-            comment_left: 1
+  static async getCommentsByParentId({
+    productId,
+    parentCommentId = null,
+    limit = 50,
+    offset = 0,
+  }) {
+    if (parentCommentId) {
+      const parent = await Comment.findById(parentCommentId);
+      if (!parent) throw new Api404Error("Not found comment for product");
+
+      return Comment.find({
+        comment_product_id: convert2ObjectId(productId),
+        comment_left: { $gt: parent.comment_left },
+        comment_right: { $lte: parent.comment_right },
+      })
+        .select({
+          comment_left: 1,
+          comment_right: 1,
+          comment_content: 1,
+          comment_parent_id: 1,
+        })
+        .sort({
+          comment_left: 1,
         });
     }
 
-    static async validateProductExists(productId) {
-        // check product exists in the database
-        const foundProduct = await ProductService.findProductById(productId)
-        if (!foundProduct) throw Api404Error('Product not found')
-    }
+    return Comment.find({
+      comment_product_id: convert2ObjectId(productId),
+      comment_parent_id: parentCommentId,
+    })
+      .select({
+        comment_left: 1,
+        comment_right: 1,
+        comment_content: 1,
+        comment_parent_id: 1,
+      })
+      .sort({
+        comment_left: 1,
+      });
+  }
 
-    static async deleteComment({
-                                   productId,
-                                   commentId
-                               }) {
-        await this.validateProductExists(productId)
+  static async validateProductExists(productId) {
+    // check product exists in the database
+    const foundProduct = await ProductService.findProductById(productId);
+    if (!foundProduct) throw new Api404Error("Product not found");
+  }
 
-        // detect left and right of commentId
-        const comment  = await Comment.findById(commentId);
-        if (!comment) throw Api404Error('Comment not found')
+  static async deleteComment({ productId, commentId }) {
+    console.log(productId, "-----------------------------");
 
-        // get left, right
-        const leftValue = comment.comment_left
-        const rightValue = comment.comment_right
+    await this.validateProductExists(productId);
 
-        // cal width
-        const width = rightValue - leftValue + 1
+    // detect left and right of commentId
+    console.log(commentId, "-----------------------------");
+    const comment = await Comment.findById(commentId);
+    if (!comment) throw new Api404Error("Comment not found");
 
-        // remove all comment id children
-        await Comment.deleteMany({
-            comment_product_id: convert2ObjectId(productId),
-            comment_left: {$gte: leftValue, $lte: rightValue}
-        })
+    // get left, right
+    const leftValue = comment.comment_left;
+    const rightValue = comment.comment_right;
 
-        // update value left and right
-        await Comment.updateMany({
-            comment_product_id: convert2ObjectId(productId),
-            comment_right: {$gt: rightValue}
-        }, {
-            $inc: {comment_right: -width}
-        })
+    // cal width
+    const width = rightValue - leftValue + 1;
 
-        await Comment.updateMany({
-            comment_product_id: convert2ObjectId(productId),
-            comment_left: {$gt: leftValue}
-        }, {
-            $inc: {comment_left: -width}
-        })
+    // remove all comment id children
+    await Comment.deleteMany({
+      comment_product_id: convert2ObjectId(productId),
+      comment_left: { $gte: leftValue, $lte: rightValue },
+    });
 
-        return true
-    }
+    // update value left and right
+    await Comment.updateMany(
+      {
+        comment_product_id: convert2ObjectId(productId),
+        comment_right: { $gt: rightValue },
+      },
+      {
+        $inc: { comment_right: -width },
+      }
+    );
+
+    await Comment.updateMany(
+      {
+        comment_product_id: convert2ObjectId(productId),
+        comment_left: { $gt: leftValue },
+      },
+      {
+        $inc: { comment_left: -width },
+      }
+    );
+
+    return true;
+  }
 }
 
-module.exports = {CommentService}
+module.exports = { CommentService };
